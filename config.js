76b5b5c032c14db4cbbb1642244ef390968a3870
onievalencia2911-beep <onievalencia2911@gmail.com>
2026-02-08 11:53:51 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://youtu.be/cieVCl6rfEY?si=6zRmF2s3zwUnTsQQ", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dlohzl2nx/video/upload/v1770521730/Bruno_Major_-_Nothing_Lyrics_sms01t.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 1.0                        // Volume level (0.0 to 1.0)
